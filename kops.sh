f08319ce17aa1bc064b237893d5ca7e6a0f23d30
aartiaws <vinkararti@gmail.com>
2025-07-16 23:27:25 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,10 +11,10 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
-aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://reyaz-kops-testbkt123.k8s.local
-kops create cluster --name reyaz.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name reyaz.k8s.local --yes --admin
+aws s3api create-bucket --bucket aarti-kops-testbkt1234.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
+aws s3api put-bucket-versioning --bucket aarti-kops-testbkt1234.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://aarti-kops-testbkt1234.k8s.local
+kops create cluster --name aarti.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops update cluster --name aarti.k8s.local --yes --admin
 
 #kops validate cluster --wait 10m
